b44b169b1d894b6a25fc5d5b4fc38823be9898ef
michele <mbarrant@iu.edu>
2023-11-21 14:13:41 -0500

    pushing projects

---
+++ Lab1/src/CalculatorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+}
